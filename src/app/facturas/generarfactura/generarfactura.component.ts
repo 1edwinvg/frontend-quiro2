f2baf3858c8f6ee8adbd4497b7d60bbd4c3f31c0
edwin <villa-edwin@hotmail.com>
2019-05-15 03:52:05 +0200

    proyecto terminado sin security

--- src/app/facturas/generarfactura/generarfactura.component.ts
+++ src/app/facturas/generarfactura/generarfactura.component.ts
@@ -1,23 +1,18 @@
-import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
-import { MatTableDataSource, MatSort, MatPaginator, MatDialog } from '@angular/material';
-import { Cliente } from 'src/app/_interface/cliente.model';
-import { RepositoryService } from 'src/app/shared/repository.service';
-import { ErrorHandlerService } from 'src/app/shared/error-handler.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { FacturaServiceId } from '../buscar-facturas-id/FacturaServiceId';
-import { facturaClienteService } from '../buscar-facturas-cliente/facturaClienteService';
-import { DetalleComponent } from '../buscar-facturas-id/detalle/detalle.component';
-import { DetallesComponent } from '../buscar-facturas-cliente/detalles/detalles.component';
-import { Factura } from 'src/app/_interface/factura.model';
-
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Cliente } from "src/app/_interface/cliente.model";
+import { RepositoryService } from "src/app/shared/repository.service";
+import { ErrorHandlerService } from "src/app/shared/error-handler.service";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Factura } from "src/app/_interface/factura.model";
+import * as jspdf from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
-  selector: 'app-generarfactura',
-  templateUrl: './generarfactura.component.html',
-  styleUrls: ['./generarfactura.component.css']
+  selector: "app-generarfactura",
+  templateUrl: "./generarfactura.component.html",
+  styleUrls: ["./generarfactura.component.css"]
 })
 export class GenerarfacturaComponent implements OnInit {
-
   public cliente: Cliente;
   public factura: Factura;
   private dialogConfig;
@@ -30,15 +25,12 @@ export class GenerarfacturaComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-  
-    this.rutaActiva.params.subscribe(
-      (params: Params) => {
-        this.idFactura = params.idProducto;
-        this.idCliente = params.idClien;
-      }
-    );
+    this.rutaActiva.params.subscribe((params: Params) => {
+      this.idFactura = params.idProducto;
+      this.idCliente = params.idClien;
+    });
     this.FacturaID();
-   this.getOwnerById();
+    this.getOwnerById();
     this.dialogConfig = {
       height: "60%",
       width: "40%",
@@ -46,41 +38,53 @@ export class GenerarfacturaComponent implements OnInit {
       data: {}
     };
   }
- 
+
   private FacturaID = () => {
-    
     //  this.idCliente = this.facturaService.getId;;
-      let ownerByIdUrl: string = `api/facturas/${this.idFactura}`;
-      this.repoService.getData(ownerByIdUrl).subscribe(
-        res => {
-          this.factura = res as Factura;
-         
-        },
-        error => {
-          this.errorService.dialogConfig = this.dialogConfig;
-          this.errorService.handleError(error);
-        }
-      );
-     
-      return this.factura;
-    };
-  
- 
-  private getOwnerById = () => {
-    
-  //  this.idCliente = this.facturaService.getId;;
-    let ownerByIdUrl: string = `api/clientes/${this.idCliente}`;
+    let ownerByIdUrl: string = `api/facturas/${this.idFactura}`;
     this.repoService.getData(ownerByIdUrl).subscribe(
       res => {
-        this.cliente = res as Cliente;
-       
+        this.factura = res as Factura;
       },
       error => {
         this.errorService.dialogConfig = this.dialogConfig;
         this.errorService.handleError(error);
       }
     );
-   
+
+    return this.factura;
+  };
+
+  private getOwnerById = () => {
+    //  this.idCliente = this.facturaService.getId;;
+    let ownerByIdUrl: string = `api/clientes/${this.idCliente}`;
+    this.repoService.getData(ownerByIdUrl).subscribe(
+      res => {
+        this.cliente = res as Cliente;
+      },
+      error => {
+        this.errorService.dialogConfig = this.dialogConfig;
+        this.errorService.handleError(error);
+      }
+    );
+
     return this.cliente;
   };
+
+  public captureScreen() {
+    var data = document.getElementById("contentToConvert");
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL("image/png");
+      let pdf = new jspdf("p", "mm", "a4"); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, "PNG", 0, position, imgWidth, imgHeight);
+      pdf.save("MYPdf.pdf"); // Generated PDF
+    });
+  }
 }
